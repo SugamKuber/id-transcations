d7e5805299b12544bf5a3a23a3a772f8c35bace9
sugamkuber <sugam7842692@gmail.com>
2023-04-02 14:27:39 +0530

    feat: adding forms

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,77 +1,93 @@
 import React from 'react'
+import axios from "axios";
+import { useAddress } from "@thirdweb-dev/react";
+import { Formik, Field } from "formik";
+import { useNavigate } from "react-router-dom";
 import {
+    useToast,
+    Box,
+    Button,
+    Flex,
     FormControl,
     FormLabel,
     FormErrorMessage,
-    Button,
-    Input
-} from '@chakra-ui/react'
-import { Field, Form, Formik } from 'formik';
-import axios from "axios";
-import { useAddress } from "@thirdweb-dev/react";
+    Input,
+    VStack
+} from "@chakra-ui/react";
 
 
 const baseURL = `https://id-transcations-production.up.railway.app/api/id/create`;
 
 export default function Register() {
-    const address = useAddress();
-
     const [post, setPost] = React.useState(null);
-
-    function validateId(value) {
-        let regex = /^\d\d[A-Z][A-Z][A-Z]\d\d\d\d$/i;
-        let error
-        if (!regex.test(value)) {
-            error = 'incorrect format'
-        }
-        return error
-    }
-
-    function createId(values) {
-        alert({
-            uniqueId: values.uniqueId,
-            publicAddress: address
-        })
-        // axios.post(baseURL, {
-        //     title: values.uniqueId,
-        //     body: "This is a new post."
-        // })
-        //     .then((response) => {
-        //         setPost(response.data);
-        //     });
-    }
-
+    const address = useAddress();
+    const navigate = useNavigate();
+    const toast = useToast()
     return (
-        <>
-            <Formik
-                onSubmit={(values) => {
-                    createId(values)
-                }}
-            >
-                {(props) => (
-                    <Form>
-                        <Field name='uniqueId' validate={validateId}>
-                            {({ field, form }) => (
-                                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                                    <FormLabel>ID</FormLabel>
-                                    <Input placeholder='reg no' />
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+        <Flex bg="gray.100" align="center" justify="center" h="100vh">
+            <Box bg="white" p={6} rounded="md" w={64}>
+                <Formik
+                    initialValues={{
+                        uniqueId: "",
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values)
+                        axios.post(baseURL, {
+                            uniqueId: values.uniqueId,
+                            publicAddress: address
+                        }).then((response) => {
+                            setPost(response.data);
+                            console.log("succesfully submitted")
+                            toast({
+                                title: 'Id registred',
+                                status: 'success',
+                                duration: 3000,
+                                isClosable: true,
+                            })
+                            navigate("/")
+
+                        }).catch(error => {
+                            console.log(error.response.data.message)
+                            toast({
+                                title: error.response.data.message,
+                                status: "error",
+                                duration: 3000,
+                                isClosable: true,
+                            })
+                        })
+                    }}
+                >
+                    {({ handleSubmit, errors, touched }) => (
+                        <form onSubmit={handleSubmit}>
+                            <VStack spacing={4} align="flex-start">
+                                <FormControl isInvalid={!!errors.uniqueId && touched.uniqueId}>
+                                    <FormLabel htmlFor="text">uniqueId</FormLabel>
+                                    <Field
+                                        as={Input}
+                                        id="uniqueId"
+                                        name="uniqueId"
+                                        type="uniqueId"
+                                        variant="filled"
+                                        validate={(value) => {
+                                            let error;
+                                            let regex = /\d\d[A-Z][A-Z][A-Z]\d\d\d\d$/i;
+                                            if (!regex.test(value)) {
+                                                error = 'incorrect format'
+                                            }
+
+                                            return error;
+                                        }}
+                                    />
+                                    <FormErrorMessage>{errors.uniqueId}</FormErrorMessage>
                                 </FormControl>
-                            )}
-                        </Field>
-                        <Button
-                            mt={4}
-                            colorScheme='teal'
-                            isLoading={props.isSubmitting}
-                            type='submit'
-                        >
-                            Submit
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-
-
-        </>
-    )
+                                <Button type="submit" colorScheme="purple" width="full">
+                                    Submit
+                                </Button>
+                            </VStack>
+                        </form>
+                    )}
+                </Formik>
+            </Box>
+        </Flex>
+    );
 }
